bbd90bb052012b7650135f5d13e0f53d8cffccad
K Anthony Marefat <marefatk@gmail.com>
2014-04-25 15:10:02 -0400

    First commit, project done

---
+++ The Project/Assets/Beam.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour {
+
+	Vector3 fwd;
+	public float splode;
+	public float rads;
+	bool fire = false;
+
+	// Use this for initialization
+	void Start () {
+
+		fwd = transform.forward;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if (Physics.Raycast(transform.position, fwd, 10))
+//			print("There is something in front of the object!");
+	
+		if (Input.GetMouseButtonDown(0)) {
+			fire = true;
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			fire = false;
+			GetComponentInChildren<MeshRenderer>().enabled=false;
+		}
+		
+		RaycastHit smash;
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, .5f));
+			if (Physics.SphereCast(ray, 10, out smash) && fire){
+				if (smash.rigidbody != null){
+					Debug.DrawLine (ray.origin, smash.point);
+					Collider[] colliders = Physics.OverlapSphere(smash.point, rads);
+					GetComponentInChildren<MeshRenderer>().enabled=!GetComponentInChildren<MeshRenderer>().enabled;
+				foreach (Collider hit in colliders) {
+					hit.rigidbody.AddExplosionForce(splode, smash.point, rads, 0);
+				
+				}
+			}
+		}
+	}
+}
