9a16dd74191c17db482150074cfb1c65fb45578c
J. Matthew Griffis <grifj153@newschool.edu>
2014-05-02 13:58:41 -0400

    Working on the cube generation.

--- The Project/Assets/scripts/makeObjects.cs
+++ The Project/Assets/scripts/makeObjects.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class makeObjects : MonoBehaviour {
 
+	GameObject[] targetList;
+	public GameObject newCube;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,11 @@ public class makeObjects : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		targetList = GameObject.FindGameObjectsWithTag("Respawn");
+		//Debug.Log(targetList.Length);
+
+		if (targetList.Length < 5) {
+			GameObject newCube = Instantiate("prefab_cube", Vector3.zero, Quaternion.identity) as GameObject;
+		}
 	}
 }
