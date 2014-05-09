8654a6eb9294459afcf3819791508964d998877e
J. Matthew Griffis <grifj153@newschool.edu>
2014-05-09 15:05:03 -0400

    Mannequins react to the beam. Need disintegration.

--- The Project/Assets/scripts/makeObjects.cs
+++ The Project/Assets/scripts/makeObjects.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class makeObjects : MonoBehaviour {
 
 	GameObject[] targetList;
-	public GameObject theCube;
+	public GameObject theCube, mann1, mann2, mann3, mann4;
 
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating("makeCube", 2, 2);
+		InvokeRepeating("makeStuff", 2, 2);
 	}
 	
 	// Update is called once per frame
@@ -16,11 +16,25 @@ public class makeObjects : MonoBehaviour {
 
 	}
 
-	void makeCube() {
+	void makeStuff() {
 		targetList = GameObject.FindGameObjectsWithTag("Respawn");
 		//Debug.Log(targetList.Length);
 		if (targetList.Length < 100) {
-			GameObject newCube = Instantiate(theCube, new Vector3(0, 10f, 0), Quaternion.identity) as GameObject;
+			GameObject tmp = theCube;
+			float die = Random.value;
+			float numSides = 5f;
+			if (die < 1f / numSides) {
+				tmp = theCube;
+			} else if (die >= 1f / numSides && die < (1f / numSides) * 2f) {
+				tmp = mann1;
+			} else if (die >= (1f / numSides) * 2f && die < (1f / numSides) * 3f) {
+				tmp= mann2;
+			} else if (die >= (1f / numSides) * 3f && die < (1f / numSides) * 4f) {
+				tmp = mann3;
+			} else if (die >= (1f / numSides) * 4f) {
+				tmp = mann4;
+			}
+			GameObject newThang = Instantiate(tmp, new Vector3(0, 10f, 0), Quaternion.identity) as GameObject;
 		}
 	}
 }
