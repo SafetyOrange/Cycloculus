bbc43f5ea52e997b8bd53db73f0a5bb7b826b602
J. Matthew Griffis <grifj153@newschool.edu>
2014-05-10 18:21:44 -0400

    Projectiles arc toward the player.

--- The Project/Assets/scripts/makeObjects.cs
+++ The Project/Assets/scripts/makeObjects.cs
@@ -9,6 +9,7 @@ public class makeObjects : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("makeStuff", 2, 2);
+		Screen.showCursor = false;
 	}
 	
 	// Update is called once per frame
@@ -19,7 +20,7 @@ public class makeObjects : MonoBehaviour {
 	void makeStuff() {
 		targetList = GameObject.FindGameObjectsWithTag("Respawn");
 		//Debug.Log(targetList.Length);
-		if (targetList.Length < 100) {
+		if (targetList.Length < 9) {
 			GameObject tmp = theCube;
 			float die = Random.value;
 			float numSides = 5f;
@@ -34,7 +35,27 @@ public class makeObjects : MonoBehaviour {
 			} else if (die >= (1f / numSides) * 4f) {
 				tmp = mann4;
 			}
-			GameObject newThang = Instantiate(tmp, new Vector3(0, 10f, 0), Quaternion.identity) as GameObject;
+			GameObject newThang = Instantiate(tmp, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+			Vector3 flyToPlayer = Camera.main.transform.position - newThang.transform.position; // Calculate the vector towards the player.
+			//Debug.Log(flyToPlayer);
+			//newThang.rigidbody.AddForce(new Vector3(0, 1000f, 0));
+			//float mult = 100f;
+			//Debug.Log(flyToPlayer.magnitude);
+			//newThang.rigidbody.AddForce(flyToPlayer * flyToPlayer.magnitude);
+			//newThang.rigidbody.AddForce(new Vector3(flyToPlayer.x, 100f, flyToPlayer.z));
+			newThang.rigidbody.velocity = BallisticVel(newThang.transform);
+			Debug.Log(newThang.rigidbody.velocity);
 		}
+	}
+
+	Vector3 BallisticVel(Transform projectile) {
+		Vector3 dir = Camera.main.transform.position - projectile.position; // get target direction
+		float h = dir.y;  // get height difference
+		dir.y = 0;  // retain only the horizontal direction
+		float dist = dir.magnitude ;  // get horizontal distance
+		dir.y = dist;  // set elevation to 45 degrees
+		dist += h;  // correct for different heights
+		float vel = Mathf.Sqrt(dist * Physics.gravity.magnitude);
+		return vel * dir.normalized;  // returns Vector3 velocity
 	}
 }
